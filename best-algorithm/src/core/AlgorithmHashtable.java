563f3215d86f894a19642f909fb62e11a7a04ed9
Tales Tenorio de Souza Pimentel <tales.tsp@gmail.com>
2014-06-19 22:58:31 -0300

    Checagem de argumentos Null feita antes da inserção e da consulta

--- best-algorithm/src/core/AlgorithmHashtable.java
+++ best-algorithm/src/core/AlgorithmHashtable.java
@@ -1,18 +1,28 @@
 package core;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class AlgorithmHashtable implements Algorithm {
-	
+
 	private HashMap<String, String> dicionario = new HashMap<String, String>();
 
-    public boolean insert(String word) {
-        dicionario.put(word, null);
-        return true;
-    }
+	Hashtable<String, String> h = new Hashtable<String, String>();
 
-    public boolean contains(String word) {
-        return dicionario.containsKey(word);
-    }
+	public boolean insert(String word) {
+		if (word != null) {
+			dicionario.put(word, null);
+		}
+		return true;
+	}
+
+	public boolean contains(String word) {
+		if (word != null) {
+			return dicionario.containsKey(word);
+		}else{
+			return false;
+		}
+
+	}
 
 }
